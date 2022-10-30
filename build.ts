f8704c245f455830c776eff07792de24fa446be3
Tanishq Kancharla <tanishqkancharla3@gmail.com>
2022-10-30 03:32:30 -0700

    esbuild and tsx

--- build.ts
+++ build.ts
@@ -1,34 +1,37 @@
 import esbuild from "esbuild";
-import fs from "node:fs";
+import fs from "node:fs/promises";
 import path from "path";
+import * as ts from "typescript";
 
-const entryPoints = fs
-	.readdirSync("./src")
-	.filter((filePath) => !filePath.endsWith(".test.ts"))
-	.map((name) => path.join("./src", name));
+function compileDts(fileNames: string[]): void {
+	const options: ts.CompilerOptions = {
+		allowJs: true,
+		declaration: true,
+		emitDeclarationOnly: true,
+		moduleResolution: ts.ModuleResolutionKind.NodeJs,
+		outDir: "dist",
+	};
 
-function buildEsm() {
-	return esbuild.build({
-		entryPoints,
-		outdir: "dist",
-		format: "esm",
-		outExtension: {
-			".js": ".mjs",
-		},
-	});
-}
-
-function buildCjs() {
-	return esbuild.build({
-		entryPoints,
-		outdir: "dist",
-		format: "cjs",
-		sourcemap: true,
-	});
+	const program = ts.createProgram(fileNames, options);
+	program.emit();
 }
 
 async function build() {
-	await Promise.all([buildEsm(), buildCjs()]);
+	const srcFiles = await fs.readdir("./src");
+
+	const entryPoints = srcFiles
+		.filter((filePath) => !filePath.endsWith(".test.ts"))
+		.map((name) => path.join(__dirname, "src", name));
+
+	await Promise.all([
+		esbuild.build({
+			entryPoints,
+			outdir: "dist",
+			format: "esm",
+			sourcemap: true,
+		}),
+		compileDts(entryPoints),
+	]);
 }
 
 build();
